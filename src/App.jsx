4571dd772cd090732180134512ef243741db39b8
Jonas-fj <jonasfjerestad@gmail.com>
2026-02-28 21:52:03 +0100

    Temp site set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import constructionImg from './assets/mathieu-stern-construction-unsplash.jpg';
+import './App.css'
+
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World!</h1>
+    <div className="construction">
+      <figure>
+        <img src={constructionImg} />
+        <figcaption> Photo by Mathieu Stern {'\u00A0'}
+          <a href="https://unsplash.com/photos/3-men-in-white-and-orange-shirts-and-green-pants-action-figures-tv7GF92ZWvs">
+            Unsplash
+          </a>
+        </figcaption>
+
+      </figure>
+      <h1>Under construction</h1>
+      <h2>Comnig to a server near you.</h2>
     </div>
   );
 }
